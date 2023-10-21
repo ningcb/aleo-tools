02f35ab6460984105a0b7203d7051ae010534247
Pranav Gaddamadugu <pranav@aleo.org>
2023-10-21 10:52:33 -0400

    Add support for authorizing private_to_public transfers

--- authorize-credits/src/lib.rs
+++ authorize-credits/src/lib.rs
@@ -5,16 +5,17 @@ use snarkvm::console::{
     types::U64,
 };
 use snarkvm::prelude::{
-    Argument, Authorization, Field, Future, Identifier, Plaintext, ProgramID, Register, Request,
-    Response, Transition, ValueType, Zero,
+    Argument, Authorization, Entry, Field, Future, Group, Identifier, Owner, Plaintext, ProgramID,
+    Record, Register, Request, Response, Transition, ValueType, Zero,
 };
 
 use anyhow::Result;
 use core::str::FromStr;
 use rand::{CryptoRng, Rng};
+use snarkvm::circuit::prelude::IndexMap;
 
 /// Returns a transaction that transfers public credits from the sender to the recipient.
-pub fn transfer_public<N: Network>(
+pub fn authorize_transfer_public<N: Network>(
     private_key: &str,
     recipient: &str,
     amount_in_microcredits: u64,
@@ -40,6 +41,17 @@ pub fn transfer_public<N: Network>(
         ValueType::from_str("address.public")?,
         ValueType::from_str("u64.public")?,
     ];
+
+    // Construct the request.
+    let request = request(
+        &private_key,
+        program_id,
+        function_name,
+        inputs,
+        input_types,
+        rng,
+    )?;
+
     // Construct the outputs.
     let outputs = vec![Value::Future(Future::new(
         ProgramID::from_str(program_id)?,
@@ -58,17 +70,7 @@ pub fn transfer_public<N: Network>(
     let output_registers = vec![Some(Register::from_str("r2")?)];
 
     // Construct the authorization.
-    let authorization = authorize(
-        &private_key,
-        program_id,
-        function_name,
-        inputs,
-        input_types,
-        outputs,
-        output_types,
-        output_registers,
-        rng,
-    )?;
+    let authorization = authorize(request, outputs, output_types, output_registers)?;
     // Get the execution ID.
     let execution_id = authorization.to_execution_id()?;
     // Authorize the fee.
@@ -82,6 +84,107 @@ pub fn transfer_public<N: Network>(
             rng,
         )?),
     };
+
+    // Return the authorizations.
+    Ok((authorization, fee_authorization))
+}
+
+/// Returns a transaction that authorizes a private to public transfer.
+fn authorize_transfer_private_to_public<N: Network>(
+    private_key: &PrivateKey<N>,
+    record_microcredits: u64,
+    record_nonce: &str,
+    recipient: &str,
+    amount_in_microcredits: u64,
+    fee_in_microcredits: u64,
+    rng: &mut (impl Rng + CryptoRng),
+) -> Result<(Authorization<N>, Authorization<N>)> {
+    // Construct the program ID and function name.
+    let (program_id, function_name) = ("credits.aleo", "transfer_private_to_public");
+    // Construct the inputs.
+    let inputs = vec![
+        Value::Record(Record::<_, Plaintext<N>>::from_plaintext(
+            Owner::Private(Plaintext::from(Literal::Address(Address::try_from(
+                private_key,
+            )?))),
+            IndexMap::from([(
+                Identifier::from_str("microcredits")?,
+                Entry::Private(Plaintext::from(Literal::U64(U64::new(record_microcredits)))),
+            )]),
+            Group::from_str(record_nonce)?,
+        )?),
+        Value::from(Literal::Address(Address::from_str(recipient)?)),
+        Value::from(Literal::U64(U64::new(amount_in_microcredits))),
+    ];
+    // Construct the input types.
+    let input_types = vec![
+        ValueType::from_str("credits.record")?,
+        ValueType::from_str("address.public")?,
+        ValueType::from_str("u64.public")?,
+    ];
+
+    // Construct the request.
+    let request = request(
+        private_key,
+        program_id,
+        function_name,
+        inputs,
+        input_types,
+        rng,
+    )?;
+
+    // Construct the outputs.
+    let outputs = vec![
+        Value::Record(Record::<_, Plaintext<N>>::from_plaintext(
+            Owner::Private(Plaintext::from(Literal::Address(Address::try_from(
+                private_key,
+            )?))),
+            IndexMap::from([(
+                Identifier::from_str("microcredits")?,
+                Entry::Private(Plaintext::from(Literal::U64(U64::new(
+                    record_microcredits - amount_in_microcredits,
+                )))),
+            )]),
+            {
+                // Prepare the index as a field element.
+                let index = Field::from_u64(4u64);
+                // Compute the randomizer as `HashToScalar(tvk || index)`.
+                let randomizer = N::hash_to_scalar_psd2(&[*request.tvk(), index])?;
+                // Compute the nonce from the randomizer.
+                N::g_scalar_multiply(&randomizer)
+            },
+        )?),
+        Value::Future(Future::new(
+            ProgramID::from_str(program_id)?,
+            Identifier::from_str(function_name)?,
+            vec![
+                Argument::Plaintext(Plaintext::from(Literal::Address(Address::from_str(
+                    recipient,
+                )?))),
+                Argument::Plaintext(Plaintext::from(Literal::U64(U64::new(
+                    amount_in_microcredits,
+                )))),
+            ],
+        )),
+    ];
+    // Construct the output types.
+    let output_types = vec![
+        ValueType::from_str("credits.record")?,
+        ValueType::from_str("credits.aleo/transfer_private_to_public.future")?,
+    ];
+    // Construct the output registers.
+    let output_registers = vec![
+        Some(Register::from_str("r4")?),
+        Some(Register::from_str("r5")?),
+    ];
+
+    // Construct the authorization.
+    let authorization = authorize(request, outputs, output_types, output_registers)?;
+    // Get the execution ID.
+    let execution_id = authorization.to_execution_id()?;
+    // Authorize the fee.
+    let fee_authorization =
+        authorize_public_fee(private_key, fee_in_microcredits, 0, execution_id, rng)?;
 
     // Return the authorizations.
     Ok((authorization, fee_authorization))
@@ -111,6 +214,17 @@ fn authorize_public_fee<N: Network>(
         ValueType::from_str("u64.public")?,
         ValueType::from_str("field.public")?,
     ];
+
+    // Construct the request.
+    let request = request(
+        private_key,
+        program_id,
+        function_name,
+        inputs,
+        input_types,
+        rng,
+    )?;
+
     // Construct the outputs.
     let outputs = vec![Value::Future(Future::new(
         ProgramID::from_str(program_id)?,
@@ -129,39 +243,20 @@ fn authorize_public_fee<N: Network>(
     // Construct the output registers.
     let output_registers = vec![Some(Register::from_str("r4")?)];
     // Construct the authorization.
-    authorize(
-        private_key,
-        program_id,
-        function_name,
-        inputs,
-        input_types,
-        outputs,
-        output_types,
-        output_registers,
-        rng,
-    )
+    authorize(request, outputs, output_types, output_registers)
 }
 
-/// An internal method that authorizes a function call with a corresponding fee.
-#[allow(clippy::too_many_arguments)]
-fn authorize<N: Network>(
+/// Constructs a request from the given inputs.
+fn request<N: Network>(
     private_key: &PrivateKey<N>,
     program_id: &str,
     function_name: &str,
     inputs: Vec<Value<N>>,
     input_types: Vec<ValueType<N>>,
-    outputs: Vec<Value<N>>,
-    output_types: Vec<ValueType<N>>,
-    output_registers: Vec<Option<Register<N>>>,
     rng: &mut (impl Rng + CryptoRng),
-) -> Result<Authorization<N>> {
+) -> Result<Request<N>> {
     // Check that the number of inputs and input types match.
     assert_eq!(inputs.len(), input_types.len());
-    let num_inputs = inputs.len();
-
-    // Check that the number of outputs, output types, and output registers match.
-    assert_eq!(outputs.len(), output_types.len());
-    assert_eq!(outputs.len(), output_registers.len());
 
     // Construct the program ID.
     let program_id = ProgramID::from_str(program_id)?;
@@ -170,24 +265,29 @@ fn authorize<N: Network>(
     let function_name = Identifier::from_str(function_name)?;
 
     // Compute the request.
-    let request = Request::sign(
+    Request::sign(
         private_key,
         program_id,
         function_name,
         inputs.into_iter(),
         &input_types,
         rng,
-    )?;
+    )
+}
 
-    // Initialize the authorization.
-    let authorization = Authorization::new(request.clone());
-
+/// Constructs a valid authorization from a request.
+fn authorize<N: Network>(
+    request: Request<N>,
+    outputs: Vec<Value<N>>,
+    output_types: Vec<ValueType<N>>,
+    output_registers: Vec<Option<Register<N>>>,
+) -> Result<Authorization<N>> {
     // Construct the response.
     let response = Response::new(
         request.network_id(),
         request.program_id(),
         request.function_name(),
-        num_inputs,
+        request.inputs().len(),
         request.tvk(),
         request.tcm(),
         outputs,
@@ -197,6 +297,9 @@ fn authorize<N: Network>(
 
     // Construct the transition manually.
     let transition = Transition::from(&request, &response, &output_types, &output_registers)?;
+
+    // Initialize the authorization.
+    let authorization = Authorization::new(request);
 
     // Add the transition to the authorization.
     authorization.insert_transition(transition)?;
@@ -209,13 +312,12 @@ fn authorize<N: Network>(
 mod test {
     use super::*;
 
+    use snarkvm::ledger::store::ConsensusStore;
     use snarkvm::prelude::store::helpers::memory::ConsensusMemory;
     use snarkvm::prelude::{Testnet3, VM};
-    use snarkvm::ledger::store::ConsensusStore;
     use snarkvm::utilities::TestRng;
 
     type CurrentNetwork = Testnet3;
-
 
     #[test]
     fn test_authorize_public() {
@@ -224,7 +326,8 @@ mod test {
         // Initialize a VM.
         let vm = VM::<CurrentNetwork, ConsensusMemory<CurrentNetwork>>::from(
             ConsensusStore::open(None).unwrap(),
-        ).unwrap();
+        )
+        .unwrap();
         // Initialize the genesis private key.
         let genesis_private_key = PrivateKey::<CurrentNetwork>::new(rng).unwrap();
         // Create the genesis block.
@@ -239,15 +342,18 @@ mod test {
         let recipient_address = Address::try_from(&recipient_private_key).unwrap();
 
         // Initialize an authorization.
-        let (authorization, fee_authorization) = transfer_public(
+        let (authorization, fee_authorization) = authorize_transfer_public(
             &sender_private_key.to_string(),
             &recipient_address.to_string(),
             100,
             10,
             rng,
-        ).unwrap();
+        )
+        .unwrap();
 
         // Execute the authorization.
-        assert!(vm.execute_authorization(authorization, fee_authorization, None, rng).is_ok());
+        assert!(vm
+            .execute_authorization(authorization, fee_authorization, None, rng)
+            .is_ok());
     }
 }
