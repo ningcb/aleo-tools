43077b51598b32dd25153d2b3c5af570cb3480a4
Pranav Gaddamadugu <23022326+d0cd@users.noreply.github.com>
2023-10-31 11:34:20 -0400

    Introduce StaticQuery

--- execute-service/src/main.rs
+++ execute-service/src/main.rs
@@ -1,3 +1,12 @@
+pub mod query;
+pub use query::*;
+
+use snarkvm::prelude::{Deserialize, Network, Serialize, StatePath, Testnet3};
+
+use anyhow::{anyhow, Result};
+
+type CurrentNetwork = Testnet3;
+
 fn main() {
     println!("Hello, world!");
 }
---
+++ execute-service/src/query.rs
@@ -0,0 +1,36 @@
+use super::*;
+
+use async_trait::async_trait;
+
+use snarkvm::ledger::query::QueryTrait;
+use snarkvm::prelude::Field;
+
+pub struct StaticQuery<N: Network> {
+    state_root: Option<N::StateRoot>,
+    state_path: Option<StatePath<N>>,
+}
+
+#[async_trait(?Send)]
+impl<N: Network> QueryTrait<N> for StaticQuery<N> {
+    fn current_state_root(&self) -> Result<N::StateRoot> {
+        self.state_root
+            .ok_or_else(|| anyhow!("State root is not set."))
+    }
+
+    async fn current_state_root_async(&self) -> Result<N::StateRoot> {
+        self.state_root
+            .ok_or_else(|| anyhow!("State root is not set."))
+    }
+
+    fn get_state_path_for_commitment(&self, _: &Field<N>) -> Result<StatePath<N>> {
+        self.state_path
+            .clone()
+            .ok_or_else(|| anyhow!("State path is not set."))
+    }
+
+    async fn get_state_path_for_commitment_async(&self, _: &Field<N>) -> Result<StatePath<N>> {
+        self.state_path
+            .clone()
+            .ok_or_else(|| anyhow!("State path is not set."))
+    }
+}
