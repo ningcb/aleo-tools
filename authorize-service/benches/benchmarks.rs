99f8617891fe788bfb7bf27e0bf383bbfbb8306f
Pranav Gaddamadugu <23022326+d0cd@users.noreply.github.com>
2023-11-01 01:49:44 -0400

    Update benchmarks

--- authorize-service/benches/benchmarks.rs
+++ authorize-service/benches/benchmarks.rs
@@ -6,7 +6,9 @@ use snarkvm::circuit::AleoV0;
 use snarkvm::prelude::{Address, Literal, PrivateKey, Process, Value, U64};
 use std::str::FromStr;
 
-use authorize_service::{authorize_transfer_public, private_key_from_seed, CurrentNetwork};
+use authorize_service::{
+    authorize_transfer_public, private_key_from_seed, AuthorizeRequest, CurrentNetwork,
+};
 
 fn bench_private_key_from_seed(c: &mut Criterion) {
     c.bench_function("private_key_from_seed", |b| {
@@ -15,15 +17,24 @@ fn bench_private_key_from_seed(c: &mut Criterion) {
 }
 
 fn bench_authorize_transfer_public(c: &mut Criterion) {
+    let request = AuthorizeRequest {
+        private_key: PrivateKey::from_str(
+            "APrivateKey1zkpCE9rCw9SixY82xaDrW2Hwxc2f3VjeuR2oZHR81zcuUDV",
+        )
+        .unwrap(),
+        recipient: Address::from_str(
+            "aleo1zcsyu7wfrdp4n6gq752p3np45sat9d6zun2uhjer2h4skccsgsgq7ndrnj",
+        )
+        .unwrap(),
+        amount_in_microcredits: U64::new(100),
+        priority_fee_in_microcredits: U64::new(0),
+    };
     c.bench_function("authorize_transfer_public", |b| {
-        b.iter(|| {
-            authorize_transfer_public::<CurrentNetwork>(
-                "APrivateKey1zkpCE9rCw9SixY82xaDrW2Hwxc2f3VjeuR2oZHR81zcuUDV",
-                " aleo1zcsyu7wfrdp4n6gq752p3np45sat9d6zun2uhjer2h4skccsgsgq7ndrnj",
-                100,
-                0,
-            )
-        })
+        b.iter_batched(
+            || request.clone(),
+            |request| authorize_transfer_public::<CurrentNetwork>(request).unwrap(),
+            BatchSize::SmallInput,
+        )
     });
 }
 
@@ -34,29 +45,28 @@ fn bench_authorize(c: &mut Criterion) {
             .unwrap();
     let program_id = "credits.aleo";
     let function_name = "transfer_public";
+    let inputs = vec![
+        Value::<CurrentNetwork>::from(Literal::Address(
+            Address::from_str("aleo1zcsyu7wfrdp4n6gq752p3np45sat9d6zun2uhjer2h4skccsgsgq7ndrnj")
+                .unwrap(),
+        )),
+        Value::from(Literal::U64(U64::new(100))),
+    ];
     let rng = &mut rand::thread_rng();
 
     c.bench_function("general_authorize", move |b| {
         b.iter_batched(
-            || {
-                vec![
-                    Value::<CurrentNetwork>::from(Literal::Address(
-                        Address::from_str(
-                            "aleo1zcsyu7wfrdp4n6gq752p3np45sat9d6zun2uhjer2h4skccsgsgq7ndrnj",
-                        )
-                        .unwrap(),
-                    )),
-                    Value::from(Literal::U64(U64::new(100))),
-                ]
-            },
+            || inputs.clone(),
             |inputs| {
-                process.authorize::<AleoV0, _>(
-                    &private_key,
-                    program_id,
-                    function_name,
-                    inputs.iter(),
-                    rng,
-                )
+                process
+                    .authorize::<AleoV0, _>(
+                        &private_key,
+                        program_id,
+                        function_name,
+                        inputs.iter(),
+                        rng,
+                    )
+                    .unwrap()
             },
             BatchSize::SmallInput,
         )
